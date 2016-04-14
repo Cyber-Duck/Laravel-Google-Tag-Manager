d9ae086af00719260072e018b36513218c2770e2
Andrew McCormack <andrewm@cyber-duck.co.uk>
2016-04-14 14:45:12 +0100

    remove tab spaces

--- src/GTM.php
+++ src/GTM.php
@@ -16,118 +16,118 @@ class GTM {
      *
      * @var GTMData
      */
-	private $data;
+    private $data;
 
     /**
      * View object
      *
      * @var GTMView
      */
-	private $view;
+    private $view;
 
-	/**
-	 * Set up the required objects for this class
-	 *
-	 * @return void
-	 */
-	public function __construct()
-	{
-		$this->data = new GTMData();
-		$this->view = new GTMView();
-	}
+    /**
+     * Set up the required objects for this class
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->data = new GTMData();
+        $this->view = new GTMView();
+    }
 
-	/**
-	 * Set the Tag Manager container ID
-	 *
-	 * @param string $id The container ID GTM-XXXXX
-	 * @return void
-	 */
-	public function id($id)
-	{
-		$this->data->setID($id);
-	}
+    /**
+     * Set the Tag Manager container ID
+     *
+     * @param string $id The container ID GTM-XXXXX
+     * @return void
+     */
+    public function id($id)
+    {
+        $this->data->setID($id);
+    }
 
-	/**
-	 * Set a dataLayer key value pair
-	 *
-	 * @param string $name  DataLayer var name
-	 * @param mixed  $value DataLayer var value
-	 * @return void
-	 */
-	public function data($name, $value)
-	{
-		$this->data->pushData($name, $value);
-	}
+    /**
+     * Set a dataLayer key value pair
+     *
+     * @param string $name  DataLayer var name
+     * @param mixed  $value DataLayer var value
+     * @return void
+     */
+    public function data($name, $value)
+    {
+        $this->data->pushData($name, $value);
+    }
 
-	/**
-	 * Push an event to the dataLayer
-	 *
-	 * @param string $name  The event name
-	 * @return void
-	 */
-	public function event($name)
-	{
-		$this->data->pushEvent($name);
-	}
+    /**
+     * Push an event to the dataLayer
+     *
+     * @param string $name  The event name
+     * @return void
+     */
+    public function event($name)
+    {
+        $this->data->pushEvent($name);
+    }
 
-	/**
-	 * Add an ecommerce transaction
-	 *
-	 * @param array $fields An array of purchase fields
-	 * @return void
-	 */
-	public function purchase($fields)
-	{
-		$this->data->pushPurchase($fields);
-	}
+    /**
+     * Add an ecommerce transaction
+     *
+     * @param array $fields An array of purchase fields
+     * @return void
+     */
+    public function purchase($fields)
+    {
+        $this->data->pushPurchase($fields);
+    }
 
-	/**
-	 * Add an ecommerce transaction item
-	 * Used in conjunction with ->purchase()
-	 *
-	 * @param array $fields An array of a purchase item fields
-	 * @return void
-	 */
-	public function purchaseItem($fields)
-	{
-		$this->data->pushPurchaseItem($fields);
-	}
+    /**
+     * Add an ecommerce transaction item
+     * Used in conjunction with ->purchase()
+     *
+     * @param array $fields An array of a purchase item fields
+     * @return void
+     */
+    public function purchaseItem($fields)
+    {
+        $this->data->pushPurchaseItem($fields);
+    }
 
-	/**
-	 * Refund an ecommerce transaction
-	 *
-	 * @param string $id The id of the transaction to refund
-	 * @return void
-	 */
-	public function refundTransaction($id)
-	{
-		$this->data->pushRefundTransaction($id);
-	}
+    /**
+     * Refund an ecommerce transaction
+     *
+     * @param string $id The id of the transaction to refund
+     * @return void
+     */
+    public function refundTransaction($id)
+    {
+        $this->data->pushRefundTransaction($id);
+    }
 
-	/**
-	 * Refund an ecommerce transaction item quantity
-	 *
-	 * @param string $id        The id of the transaction
-	 * @param string $productId The id of the item
-	 * @param int    $quantity  The quantity to refund
-	 *
-	 * @return void
-	 */
-	public function refundItem($id, $productId, $quantity)
-	{
-		$this->data->pushRefundTransactionItem($id, $productId, $quantity);
-	}
+    /**
+     * Refund an ecommerce transaction item quantity
+     *
+     * @param string $id        The id of the transaction
+     * @param string $productId The id of the item
+     * @param int    $quantity  The quantity to refund
+     *
+     * @return void
+     */
+    public function refundItem($id, $productId, $quantity)
+    {
+        $this->data->pushRefundTransactionItem($id, $productId, $quantity);
+    }
 
-	/**
-	 * Return the formatted dataLayer code
-	 *
-	 * @return string
-	 */
-	public function code()
-	{
-		return $this->view->make(
-			$this->data->getID(),
-			$this->data->getDataLayer()
-		);
-	}
+    /**
+     * Return the formatted dataLayer code
+     *
+     * @return string
+     */
+    public function code()
+    {
+        return $this->view->make(
+            $this->data->getID(),
+            $this->data->getDataLayer()
+        );
+    }
 }
--- src/GTMView.php
+++ src/GTMView.php
@@ -11,27 +11,27 @@ namespace CyberDuck\LaravelGoogleTagManager;
 
 class GTMView {
 
-	/**
-	 * Return the Tag Manager code
-	 *
-	 * @param string $id   The container ID
-	 * @param string $data The dataLayer
-	 * @return string
-	 */
-	public function make($id, $data)
-	{
-		return '<script>
-		window.dataLayer = window.dataLayer || [];
-		'.$data.'
-		<!-- Google Tag Manager -->
-		<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-'.$id.'"
-		height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({\'gtm.start\':
-		new Date().getTime(),event:\'gtm.js\'});var f=d.getElementsByTagName(s)[0],
-		j=d.createElement(s),dl=l!=\'dataLayer\'?\'&l=\'+l:\'\';j.async=true;j.src=
-		\'//www.googletagmanager.com/gtm.js?id=\'+i+dl;f.parentNode.insertBefore(j,f);
-		})(window,document,\'script\',\'dataLayer\',\'GTM-'.$id.'\');</script>
-		<!-- End Google Tag Manager -->
-		</script>';
-	}
+    /**
+     * Return the Tag Manager code
+     *
+     * @param string $id   The container ID
+     * @param string $data The dataLayer
+     * @return string
+     */
+    public function make($id, $data)
+    {
+        return '<script>
+        window.dataLayer = window.dataLayer || [];
+        '.$data.'
+        <!-- Google Tag Manager -->
+        <noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-'.$id.'"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({\'gtm.start\':
+        new Date().getTime(),event:\'gtm.js\'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!=\'dataLayer\'?\'&l=\'+l:\'\';j.async=true;j.src=
+        \'//www.googletagmanager.com/gtm.js?id=\'+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,\'script\',\'dataLayer\',\'GTM-'.$id.'\');</script>
+        <!-- End Google Tag Manager -->
+        </script>';
+    }
 }
